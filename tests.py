ca1f3bd450b7095ec6d6f52d908f8bb8d0ff9ac9
Auzzy <metalnut4@netscape.net>
2013-05-19 09:39:08 -0400

    Fixed up some of the tests.

--- tests.py
+++ tests.py
@@ -1,4 +1,11 @@
-def test1(x, y):
+def test1():
+	num = 4+6
+	print num
+	num2 = 9
+	del num
+	print num2
+
+def test2(x, y):
 	num = 8
 	if x != y:
 		if 2*x == x+10:
@@ -6,13 +13,6 @@ def test1(x, y):
 		else:
 			num = 4
 	return num
-
-def test2():
-	num = 4+6
-	print num
-	num2 = 9
-	del num
-	print num2
 
 def test3(x):
 	num = 1
@@ -26,38 +26,7 @@ def test3(x):
 		print "HEY"
 	num2 = 6
 
-def test4():
-	i = 2
-	while i<10:
-		print i
-		i += 2
-
-def test5():
-	try:
-		temp = 7
-		print temp
-		print num3
-		print
-		test = 2
-	except NameError,mess:
-		num2 = 8
-		print
-	except TypeError:
-		num2 = 7
-	print "hi"
-
-def test6(num, num2=12, *num3, **num4):
-	del num2
-	return num
-
-def test7(num, num2=12, *num3, **num4):
-	if num==4:
-		return "HI!"
-	else:
-		pass
-	return None
-
-def test8(num, num2):
+def test4(num, num2):
 	if num==3:
 		if num2:
 			print "here"
@@ -66,22 +35,12 @@ def test8(num, num2):
 	else:
 		print "HI!"
 
-def test9():
-	try:
-		try:
-			print num
-		except NameError,mess:
-			print "HERE"
-		4/0
-	except ZeroDivisionError,mess:
-		print "yup!"
-
-def test10(num):
+def test5(num):
 	if num==4:
 		return 8
 	num2 = 0
 
-def test11(x, y):
+def test6(x, y):
 	num = 8
 	if x!=y:
 		if 2*x==x+10:
@@ -91,13 +50,13 @@ def test11(x, y):
 				num = 4
 	return num
 
-def test12(x, y):
+def test7(x, y):
 	z = y
 	if x==z:
 		if y==x+10:
 			print "ERROR"
 
-def test13(x, y):
+def test8(x, y):
 	if x!=y:
 		if 2*x==x+10:
 			pass
@@ -105,11 +64,7 @@ def test13(x, y):
 			if y==7:
 				pass
 
-def test14(x, y):
-	while x<y:
-		x = x+1
-
-def test15(x, y):
+def test9(x, y):
 	if x!=y:
 		print "x != y "
 		if 2*x==x+10:
